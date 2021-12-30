a40695929b7e482bc3dbd55bb136aaaf468891c9
damircp <cdcaro94@misena.edu.co>
2021-12-30 09:30:19 -0500

    Damir seguimiento de la app

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
+        OwenIt\Auditing\AuditingServiceProvider::class,
 
     ],
 
--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -53,7 +53,7 @@
 
                                                   <label class="form-check-label">
 
-                                                    <input class="form-check-input" type="checkbox" name="permissions[]" value="{{ $permission->id }}">
+                                                    <input  class="form-check-input" type="checkbox" name="permissions[]" value="{{ $permission->id }}">
 
                                                     <span class="form-check-sign">
                                                       <span class="check"></span>
@@ -74,7 +74,7 @@
                                   </div></td>
                             </tr>
                             @endforeach
-                           
+
                           </tbody>
                         </table>
                       </div>
